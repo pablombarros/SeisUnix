e96da5c6ff3eff1d6614d66dd7257f0490e575b3
l2r-git <80277078+l2r-git@users.noreply.github.com>
2021-08-14 15:00:23 -0700

    Add files via upload

---
+++ src/demos/Geom3D/Subincsv/Clean.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+# Clean.sh - Remove files created by subincsv demos
+# Andre Latour, Aug  2021
+
+# Remove files created by subincsp tests     
+
+rm -f k1.csv k2.csv faketfnb.su faketfnb2.su fakeEFG.su 
+rm -f fakeEGF.su fakeFEH.su fakeFHE.su fakeGEH.su fakeGHE.su 
+rm -f fakeHFG.su fakeHGF.su faketfb.su faketfbs.su faketfbss.su
+rm -f fakepoststack.su fakeallxsrnp.su
+
+exit 0
