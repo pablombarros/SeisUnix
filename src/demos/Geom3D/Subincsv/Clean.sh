d9d9cc65d928bf6ef7761252f32d187a68018d0e
l2r-git <80277078+l2r-git@users.noreply.github.com>
2021-10-05 12:48:53 -0700

    change to use self generated traces in tests

--- src/demos/Geom3D/Subincsv/Clean.sh
+++ src/demos/Geom3D/Subincsv/Clean.sh
@@ -4,7 +4,12 @@
 
 # Remove files created by subincsp tests     
 
-rm -f k1.csv k2.csv
-rm -f *.su
+rm -f faketfn.su fakex.su fakexs.su fakexsr.su fakexsrf.su
+rm -f fake0x.su fake0xs.su fake0xsr.su k1.csv  
+rm -f faketfnb.su faketfnb2.su   
+rm -f fakeEFG.su fakeEGF.su fakeFEH.su fakeFHE.su   
+rm -f fakeGEH.su fakeGHE.su fakeHFG.su fakeHGF.su 
+rm -f faketfb.su faketfbs.su faketfbss.su 
+rm -f fakepoststack.su k2.csv fake0xsrp.su
 
 exit 0
