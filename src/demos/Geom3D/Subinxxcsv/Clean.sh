6cbbacc39d3a63c98196906cb5177fd89666fe3f
l2r-git <80277078+l2r-git@users.noreply.github.com>
2021-12-22 15:40:37 -0800

    examples for subinxxscv

---
+++ src/demos/Geom3D/Subinxxcsv/Clean.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+# Clean.sh - Remove files created by subinxxcsv demos
+# Andre Latour, Dec 2021
+
+# Remove files created by subinxxcsv tests     
+
+rm -f kEFGout1.csv
+rm -f kEFGout2.csv
+rm -f q1.csv
+rm -f fake1.su
+rm -f fake2.su
+
+exit 0
