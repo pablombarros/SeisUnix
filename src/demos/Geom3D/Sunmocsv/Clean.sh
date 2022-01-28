904e18531e3ff66b007eb00306f1a10cc067d89f
l2r <80277078+l2r-git@users.noreply.github.com>
2022-01-28 15:31:18 -0800

    Add tests for using velocities from q-files.
    Also modified previous tests for easier comparisons between using velocities via command line and via q-files.

--- src/demos/Geom3D/Sunmocsv/Clean.sh
+++ src/demos/Geom3D/Sunmocsv/Clean.sh
@@ -4,10 +4,8 @@
 
 # Remove files created by sunmocsp tests     
 
-rm -f fakex.su fakexs.su fakexsr.su fakexsrf.su 
-rm -f fakexsrfn.su fakexsrfng.su fakexsrfngs.su 
-rm -f fakexsrfngsn.su fakexsrfngsns.su tnull.su 
-rm -f tnullcdp.su tinfo.txt tnullvels 
-rm -f tnullcdpnmo.su tnullvels.txt
+rm -f fake* 
+rm -f tinfo* 
+rm -f tnull* 
 
 exit 0
